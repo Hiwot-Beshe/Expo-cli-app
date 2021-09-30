d4e3d1fcf29306b97c0148dc0a2293db15349d10
Hiwot-Beshe <beshehiwot@gmail.com>
2021-09-29 17:11:51 -0700

    changes the style and some containte

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hiwot</Text>
-      <StatusBar style="auto" />
+      
+     
+      <View style={styles.blue}>
+        <Text   style={{color: '#888', fontSize: 18}}>Home</Text>
+        <Text style={{color: '#888', fontSize: 18}}>About</Text>
+        <Text style={{color: '#888', fontSize: 18}}>contact</Text>
+        
+      </View>
+      <View style={styles.red}>
+        <Text style={{ fontSize: 36}}>Hiwot Beshe</Text>
+        
+      </View>
+      <View style={styles.black}>
+        <Text  style={{color: '#888', fontSize: 18}}>Hiwot</Text>
+       
+      </View>
     </View>
   );
 }
@@ -14,8 +28,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'',
     backgroundColor: '#fff',
+    
+    
+  },
+  blue: {
+    flex: 1,
+    flexDirection:'row',
+    width:'100%',
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    alignContent:'space-between',
+    justifyContent: "space-evenly",
+    
+   
+    
+  },
+  red: {
+    flex: 5,
+    backgroundColor: 'darkblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  black: {
+    flex: 1,
+    backgroundColor: 'steelblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    fontStyle:'italic',
+  
+   
+  },
+ 
+
 });
