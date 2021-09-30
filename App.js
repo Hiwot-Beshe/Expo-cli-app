62284c61053ab20b96e3f63f74ae99a18426becc
Hiwot-Beshe <beshehiwot@gmail.com>
2021-09-29 18:53:19 -0700

    Add picture

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Image, StyleSheet, Text,  View } from 'react-native';
+import Profile1 from './assets/profile1.JPG'; 
 
 export default function App() {
   return (
@@ -15,10 +16,11 @@ export default function App() {
       </View>
       <View style={styles.red}>
         <Text style={{ fontSize: 36}}>Hiwot Beshe</Text>
+        <Image source={Profile1} style={{ width: 50, height: 50 }} /> 
         
       </View>
       <View style={styles.black}>
-        <Text  style={{color: '#888', fontSize: 18}}>Hiwot</Text>
+        <Text  style={{color: 'gray', fontSize: 18}}>Hiwot</Text>
        
       </View>
     </View>
